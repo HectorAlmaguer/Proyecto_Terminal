a228eccf2850b4912b9b9d89114a24492a5c5463
HectorAlmaguer <hectorm.almaguerc@gmail.com>
2024-03-21 20:50:53 -0600

    Colonia Add Location

--- scripts/map.js
+++ scripts/map.js
@@ -1,9 +1,20 @@
 const btn_location = document.querySelector("#location-button");
+const location_data = document.querySelector("#mapa");
+
+
+const get_address = async (latitude,longitude) => {
+  const response = await fetch("https://nominatim.openstreetmap.org/reverse?lat="+latitude+"&lon="+longitude+"&format=json");
+  const data = await response.json();
+  const element = document.createElement("h2");
+      element.textContent = data.address.neighbourhood
+      location_data.appendChild(element);
+}
 
 function get_location() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log(position);
+      get_address(position.coords.latitude,position.coords.longitude);
     });
   }
 }
