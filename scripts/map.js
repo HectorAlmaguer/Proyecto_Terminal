515c7e8ed54c6261cd7d964ca096b22126752bd6
HectorAlmaguer <hectorm.almaguerc@gmail.com>
2024-06-26 15:00:24 -0600

    Fix Added:Function Follow map

--- scripts/map.js
+++ scripts/map.js
@@ -1,106 +1,8 @@
-const url_DB = "https://proyecto-ipn-default-rtdb.firebaseio.com/Crimes.json";
+const url_DB_buttons =
+  "https://proyecto-ipn-default-rtdb.firebaseio.com/botones.json";
+const url_DB_crimes =
+  "https://proyecto-ipn-default-rtdb.firebaseio.com/Crimes.json";
 
-const btn_location = document.querySelector("#location-button");
-const location_data = document.querySelector("#neighbourhood");
-const table_crimes = document.querySelector("#table_crimes");
-
-function danger_alert() {
-  swal({
-    icon: "error",
-    title: "Cuidado, estás en una zona con un índice alto de robos",
-    content: {
-      element: "div",
-      attributes: {
-        innerHTML: `
-          <p>Si notas conductas fuera de lugar, tal como personas que te están siguiendo o que intentan acercarse a ti inesperadamente, trata de incorporarte a un lugar concurrido o cambiar tu trayectoria</p>
-          <ul>
-            <li>Evita uso de tu teléfono o audífonos</li>
-            <li>Evita tener a la vista objetos de valor</li>
-            <li>Evita sitios oscuros y solitarios</li>
-          </ul>
-        `,
-      },
-    },
-  });
-}
-
-function safe_alert() {
-  swal({
-    icon: "success",
-    title: "Estás en una zona con un índice bajo de robos",
-    content: {
-      element: "div",
-      attributes: {
-        innerHTML: `
-          <ul>
-            <li>Evita uso de tu teléfono o audífonos</li>
-            <li>Evita tener a la vista objetos de valor</li>
-            <li>Evita sitios oscuros y solitarios</li>
-          </ul>
-        `,
-      },
-    },
-  });
-}
-
-function warning_alert() {
-  swal({
-    icon: "warning",
-    title: "Estás en una zona con un índice medio de robos",
-    content: {
-      element: "div",
-      attributes: {
-        innerHTML: `
-          <p>Si notas conductas fuera de lugar, tal como personas que te están siguiendo o que intentan acercarse a ti inesperadamente, trata de incorporarte a un lugar concurrido o cambiar tu trayectoria</p>
-          <ul>
-            <li>Evita uso de tu teléfono o audífonos</li>
-            <li>Evita tener a la vista objetos de valor</li>
-            <li>Evita sitios oscuros y solitarios</li>
-          </ul>
-        `,
-      },
-    },
-  });
-}
-
-function filter_data(lat, lon) {
-  const filterList = [];
-  const lat_target = lat;
-  const lon_target = lon;
-  for (let index = 0; index < crimesList.length; index++) {
-    const difference_lat = Math.abs(
-      crimesList[index].latitud_delito - lat_target
-    );
-    const difference_lon = Math.abs(
-      crimesList[index].longitud_delito - lon_target
-    );
-    if (difference_lat <= 0.0025 && difference_lon <= 0.0025) {
-      filterList.push(crimesList[index]);
-    }
-  }
-  renderList(filterList);
-  const numCrimes = filterList.length;
-
-  if (numCrimes < 25) {
-    safe_alert();
-  } else if (numCrimes >= 25 && numCrimes <= 55) {
-    warning_alert();
-  } else {
-    danger_alert();
-  }
-}
-
-function getMapID(lat, lon, mapid) {
-  let map = L.map(mapid).setView([lat, lon], 16);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-
-  L.marker([lat, lon]).addTo(map);
-}
-
-// Definir los límites de la Ciudad de México
 const CDMX_BOUNDS = {
   north: 19.592757,
   south: 19.189715,
@@ -117,126 +19,22 @@ function isWithinCDMX(lat, lon) {
   );
 }
 
-const get_address = async (latitude, longitude) => {
-  const response = await fetch(
-    "https://nominatim.openstreetmap.org/reverse?lat=" +
-      latitude +
-      "&lon=" +
-      longitude +
-      "&format=json"
-  );
-  const data = await response.json();
-  const element = document.querySelector("#element");
-  if (isWithinCDMX(latitude, longitude)) {
-    filter_data(latitude, longitude);
-    element.textContent = data.address.neighbourhood;
-    element.className = "text-center";
-  } else {
-    swal({
-      icon: "error",
-      title: "Ubicación fuera de la Ciudad de México",
-      text: "La aplicación solo muestra información de la Ciudad de México.",
-    });
+const parserResponseFireBaseButtons = (response) => {
+  const parsedResponse = [];
+  for (const key in response) {
+    const element = {
+      id: key,
+      Alcaldia: response[key].Alcaldía,
+      Colonia: response[key].Colonia,
+      latitud_boton: response[key].Latitud,
+      longitud_boton: response[key].Longitud,
+    };
+    parsedResponse.push(element);
   }
+  return parsedResponse;
 };
 
-function startTracking() {
-  let watchId;
-
-  function success(position) {
-    get_address(position.coords.latitude, position.coords.longitude);
-
-    let map = L.map("map").setView(
-      [position.coords.latitude, position.coords.longitude],
-      16
-    );
-
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-
-    L.marker([position.coords.latitude, position.coords.longitude], {
-      icon: L.icon({
-        iconUrl: "../media/gps_icon.webp",
-        shadowUrl:
-          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
-      }),
-    })
-      .addTo(map)
-      .bindPopup("Estás aquí")
-      .openPopup();
-  }
-
-  function error(e) {
-    swal({
-      icon: "error",
-      title: "Oops...",
-      text: "Debes permitir el acceso a la ubicación para mostrar información.",
-    });
-  }
-
-  if ("geolocation" in navigator) {
-    watchId = navigator.geolocation.watchPosition(success, error, {
-      enableHighAccuracy: true,
-      timeout: 10000,
-      maximumAge: 0,
-    });
-  } else {
-    console.error("Geolocation is not supported by this browser.");
-  }
-}
-
-const renderCrimes = (crime, index) => {
-  const card_container = document.createElement("div");
-  const map_crime = document.createElement("div");
-  const img = document.createElement("img");
-  const card_body = document.createElement("div");
-  const card_title = document.createElement("h5");
-  const list_group = document.createElement("ul");
-  const list_group_item1 = document.createElement("li");
-  const list_group_item2 = document.createElement("li");
-  const list_group_item3 = document.createElement("li");
-
-  card_container.className = "card m-3";
-  card_container.style = "width: 18rem";
-  img.className = "card-img-top";
-  let mapid = `map_crime${index}`;
-  map_crime.setAttribute("id", mapid);
-  card_body.className = "card-body";
-  card_title.className = "card-title text-center";
-  list_group.className = "list-group list-group-flush";
-  list_group_item1.className = "list-group-item";
-  list_group_item2.className = "list-group-item";
-  list_group_item3.className = "list-group-item";
-
-  card_title.textContent = crime.delito;
-  list_group_item3.textContent = `${crime.fecha_hecho} ${crime.hora_hecho}`;
-  list_group_item2.textContent = `${crime.latitud_delito}, ${crime.longitud_delito}`;
-  list_group_item1.textContent = `${crime.colonia_catalogo}, ${crime.alcaldia_catalogo}`;
-
-  table_crimes.appendChild(card_container);
-  card_container.appendChild(map_crime);
-  getMapID(crime.latitud_delito, crime.longitud_delito, mapid);
-  card_container.appendChild(card_body);
-  card_body.appendChild(card_title);
-  card_container.appendChild(list_group);
-  list_group.appendChild(list_group_item1);
-  list_group.appendChild(list_group_item2);
-  list_group.appendChild(list_group_item3);
-};
-
-const cleanList = () => {
-  while (table_crimes.firstChild) {
-    table_crimes.removeChild(table_crimes.firstChild);
-  }
-};
-
-const parserResponseFireBase = (response) => {
+const parserResponseFireBaseCrimes = (response) => {
   const parsedResponse = [];
   for (const key in response) {
     const element = {
@@ -256,33 +54,187 @@ const parserResponseFireBase = (response) => {
   return parsedResponse;
 };
 
-const renderList = (listToRender) => {
-  console.log(listToRender);
-  listToRender.forEach((crime, index) => {
-    renderCrimes(crime, index);
-  });
-};
-
-const getInfoApi = async () => {
+async function load_buttons_list() {
   try {
-    const response = await fetch(url_DB, {
-      method: "GET",
-    });
+    const response = await fetch(url_DB_buttons, { method: "GET" });
+
+    if (!response.ok) {
+      throw new Error(
+        `Error al cargar la lista de botones de pánico: ${response.statusText}`
+      );
+    }
+
     const parsed = await response.json();
-    const array_crimes = parserResponseFireBase(parsed);
-    crimesList = array_crimes;
-    console.log(crimesList);
-    cleanList();
+    console.log("Datos recibidos de la API:", parsed);
+
+    const buttonsList = parserResponseFireBaseButtons(parsed);
+    return buttonsList;
+  } catch (error) {
+    throw new Error(
+      `Error al procesar la respuesta de la API: ${error.message}`
+    );
+  }
+}
+
+async function load_crimes_list() {
+  try {
+    const response = await fetch(url_DB_crimes, { method: "GET" });
+
+    if (!response.ok) {
+      throw new Error(
+        `Error al cargar la lista de crímenes: ${response.statusText}`
+      );
+    }
+
+    const parsed = await response.json();
+    console.log("Datos recibidos de la API:", parsed);
+
+    const crimesList = parserResponseFireBaseCrimes(parsed);
+    return crimesList;
+  } catch (error) {
+    throw new Error(
+      `Error al procesar la respuesta de la API: ${error.message}`
+    );
+  }
+}
+
+function danger_alert() {
+  swal({
+    icon: "error",
+    title: "Cuidado, estás en una zona con un índice alto de robos",
+    content: {
+      element: "div",
+      attributes: {
+        innerHTML: `
+            <p>Si notas conductas fuera de lugar, tal como personas que te están siguiendo o que intentan acercarse a ti inesperadamente, trata de incorporarte a un lugar concurrido o cambiar tu trayectoria</p>
+            <ul>
+              <li>Evita uso de tu teléfono o audífonos</li>
+              <li>Evita tener a la vista objetos de valor</li>
+              <li>Evita sitios oscuros y solitarios</li>
+            </ul>
+          `,
+      },
+    },
+  });
+}
+
+function safe_alert() {
+  swal({
+    icon: "success",
+    title: "Estás en una zona con un índice bajo de robos",
+    content: {
+      element: "div",
+      attributes: {
+        innerHTML: `
+            <ul>
+              <li>Evita uso de tu teléfono o audífonos</li>
+              <li>Evita tener a la vista objetos de valor</li>
+              <li>Evita sitios oscuros y solitarios</li>
+            </ul>
+          `,
+      },
+    },
+  });
+}
+
+function warning_alert() {
+  swal({
+    icon: "warning",
+    title: "Estás en una zona con un índice medio de robos",
+    content: {
+      element: "div",
+      attributes: {
+        innerHTML: `
+            <p>Si notas conductas fuera de lugar, tal como personas que te están siguiendo o que intentan acercarse a ti inesperadamente, trata de incorporarte a un lugar concurrido o cambiar tu trayectoria</p>
+            <ul>
+              <li>Evita uso de tu teléfono o audífonos</li>
+              <li>Evita tener a la vista objetos de valor</li>
+              <li>Evita sitios oscuros y solitarios</li>
+            </ul>
+          `,
+      },
+    },
+  });
+}
+
+async function initialize_map() {
+  const map = L.map("map").setView([19.4326018, -99.1332049], 21);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  let currentCircleColor = "";
+
+  try {
+    const buttonsList = await load_buttons_list();
+    const crimesList = await load_crimes_list();
+
+    map.on('moveend', async function() {
+      const bounds = map.getBounds();
+      const southwest = bounds.getSouthWest();
+      const northeast = bounds.getNorthEast();
+
+      const visibleArea = {
+        minLat: southwest.lat,
+        maxLat: northeast.lat,
+        minLon: southwest.lng,
+        maxLon: northeast.lng
+      };
+
+      const filteredCrimes = crimesList.filter(crime => {
+        return (
+          crime.latitud_delito >= visibleArea.minLat &&
+          crime.latitud_delito <= visibleArea.maxLat &&
+          crime.longitud_delito >= visibleArea.minLon &&
+          crime.longitud_delito <= visibleArea.maxLon
+        );
+      });
+
+      const numCrimes = filteredCrimes.length;
+      let circleColor = "#ff0000";
+
+      if (numCrimes < 25) {
+        circleColor = "#00ff00";
+      } else if (numCrimes >= 25 && numCrimes <= 55) {
+        circleColor = "#ffff00";
+      }
+
+      if (currentCircleColor !== circleColor) {
+        currentCircleColor = circleColor;
+        if (circleColor === "#00ff00") {
+          safe_alert();
+        } else if (circleColor === "#ffff00") {
+          warning_alert();
+        } else {
+          danger_alert();
+        }
+      }
+
+      // Limpia los marcadores de crímenes antes de añadir los nuevos
+      map.eachLayer(layer => {
+        if (layer instanceof L.Marker) {
+          map.removeLayer(layer);
+        }
+      });
+
+      filteredCrimes.forEach(crime => {
+        const { latitud_delito, longitud_delito } = crime;
+        L.marker([latitud_delito, longitud_delito])
+          .addTo(map)
+          .bindPopup(crime.delito);
+      });
+    });
   } catch (error) {
     swal({
       icon: "error",
       title: "Oops...",
-      text: "Error inesperado",
+      text: error.message || "Error inesperado",
     });
   }
-};
+}
 
-let crimesList = [];
 document
   .getElementById("update-database-button")
   .addEventListener("click", async () => {
@@ -315,12 +267,5 @@ document
     }
   });
 
-btn_location.addEventListener("click", (event) => {
-  clickeo();
-});
-
-async function clickeo() {
-  let get_data = await getInfoApi();
-  get_data;
-  startTracking();
-}
+// Llamar a la función principal para inicializar el mapa y cargar la información
+initialize_map();
